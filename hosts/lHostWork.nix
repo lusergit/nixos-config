e3aa2a1f877f84c30fe3281ba7e779df4cca1b85
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-01-10 15:39:57 +0100

    chore(format): remove auto generated comments
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- hosts/lHostWork.nix
+++ hosts/lHostWork.nix
@@ -104,12 +104,7 @@
 
   swapDevices = [ { device = "/dev/disk/by-uuid/b1533042-571d-4b28-a967-6a8a0c713d46"; } ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
