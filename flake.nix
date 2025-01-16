b2a6a958b632e79620bc18871e7fdd410a2e1bc8
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-01-16 12:02:16 +0100

    chore(rollback): rollback to 24.11
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "lHosts + devenv";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
