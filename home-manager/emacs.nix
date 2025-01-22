f8fe6e4aea035bfefcf112e781f10de98cf5eff7
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-01-22 11:38:06 +0100

    chore(emacs): elixir lang servers
    Multiple language servers in emacs wrapper: `elixir-ls`, `lexical` and `next-ls`
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -1,18 +1,21 @@
 { pkgs, lib, ... }:
+let
+  language-servers = with pkgs; [ lexical next-ls elixir-ls ];
+in
 {
 
   programs.emacs = {
     enable = true;
-    package = pkgs.symlinkJoin {
+    package = (pkgs.symlinkJoin {
       name = "${lib.getName pkgs.emacs}-wrapped-${lib.getVersion pkgs.emacs}";
       paths = [ pkgs.emacs ];
       preferLocalBuild = true;
       nativeBuildInputs = [ pkgs.makeWrapper ];
       postBuild = ''
         wrapProgram $out/bin/emacs \
-          --prefix PATH : ${lib.makeBinPath [ pkgs.lexical ]}
+          --prefix PATH : ${lib.makeBinPath language-servers}
       '';
-    };
+    });
   };
 
   services.emacs = {
